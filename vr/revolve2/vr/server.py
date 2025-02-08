9f8ea423d1974450f49cd1016a6148b1d7768df4
JianlongLei <leijianlong.jalon@gmail.com>
2025-02-08 16:41:55 +0100

    update three experiments

--- vr/revolve2/vr/server.py
+++ vr/revolve2/vr/server.py
@@ -15,58 +15,36 @@ class Revolve2Server:
         self.handler = None
         self.control_commands = []
         self.brains_data = []
+        self.spliter = '<revolve2_spliter>'
 
 
     def send_mujoco_xml(self, mujoco_xml:str):
         if self.conn:
             # xml_data = {"type": "send_xml", "data": {"xml_data": mujoco_xml}}
             print(f"Sending mujoco xml: {mujoco_xml[:100]}")
-            self.conn.sendall(mujoco_xml.encode('utf-8'))
+            self.do_send_message(mujoco_xml.decode('utf8'))
             # self.conn.send(mujoco_xml)
 
-    # 处理客户端请求
+    def do_send_message(self, message, conn = None):
+        if not conn:
+            conn = self.conn
+        conn.sendall((message + self.spliter).encode('utf-8'))
+
+
     def handle_client(self, conn, addr):
         print(f"Connected by {addr}")
         self.conn = conn
-
+        buffer = ""
         while True:
             try:
-                # 接收消息
                 data = conn.recv(655360).decode('utf-8')
                 if not data:
                     break
-                print("received data:", data[:500])
-                message = json.loads(data)
-                if self.handler:
-                    self.handler(message)
-                print(message)
-                msg_type = message.get("type")
-                msg_data = message.get("data")
-
-                # 根据 type 执行不同逻辑
-                if msg_type == "request_experiment":
-                    print("Received experiment request:", msg_data)
-
-                    if msg_data['msg'] == 'exp_1':
-                        self.run_experiment_1()
-                    elif msg_data['msg'] == 'exp_2':
-                        self.run_experiment_2()
-                    elif msg_data['msg'] == 'exp_3':
-                        self.run_experiment_3()
-
-                elif msg_type == "send_experiment_data":
-                    print("Received experiment data:", msg_data)
-
-                elif msg_type == "control_command":
-                    print("Control command received (unexpected at this step).")
-                    # self.send_control_commands()
-
-                elif msg_type == "stop_server":
-                    print("Stopping server.")
-                    response = {"type": "stop_server", "success": True}
-                    conn.sendall(json.dumps(response).encode('utf-8'))
-                else:
-                    print("Unknown message type:", msg_type)
+                buffer += data
+                while self.spliter in buffer:
+                    message, buffer = buffer.split(self.spliter, 1)
+                    if message:
+                        self.process_message(json.loads(message), conn)
 
             except Exception as e:
                 print(f"Error: {e}")
@@ -75,12 +53,45 @@ class Revolve2Server:
         conn.close()
         print(f"Connection with {addr} closed.")
 
+    def process_message(self, message, conn):
+        if self.handler:
+            self.handler(message)
+        print(message)
+        msg_type = message.get("type")
+        msg_data = message.get("data")
+
+        # 根据 type 执行不同逻辑
+        if msg_type == "request_experiment":
+            print("Received experiment request:", msg_data)
+
+            if msg_data['msg'] == 'exp_1':
+                self.run_experiment_1()
+            elif msg_data['msg'] == 'exp_2':
+                self.run_experiment_2()
+            elif msg_data['msg'] == 'exp_3':
+                self.run_experiment_3()
+
+        elif msg_type == "send_experiment_data":
+            print("Received experiment data:", msg_data)
+
+        elif msg_type == "control_command":
+            print("Control command received (unexpected at this step).")
+            # self.send_control_commands()
+
+        elif msg_type == "stop_server":
+            print("Stopping server.")
+            response = {"type": "stop_server", "success": True}
+            self.do_send_message(json.dumps(response), conn)
+        else:
+            print("Unknown message type:", msg_type)
+
+
     def append_control_command(self, ctrl_index_position, position, ctrl_index_velocity, velocity):
         self.control_commands.append({"ctrl_index_position": ctrl_index_position, "position": position,
                          "ctrl_index_velocity": ctrl_index_velocity, "velocity": velocity
                                       })
 
-    # 定时发送控制命令
+    # send control command
     def send_control_commands(self):
         control_message = {
             "type": "control_command",
@@ -88,7 +99,7 @@ class Revolve2Server:
         }
         print(f"Sending control command: {control_message}")
         json_message = json.dumps(control_message)
-        self.conn.sendall(json_message.encode('utf-8'))
+        self.do_send_message(json_message)
         self.control_commands.clear()
 
 
@@ -104,10 +115,9 @@ class Revolve2Server:
             }
         }
         print(f"Sending brain message: {brain_message}")
-        self.conn.sendall(json.dumps(brain_message).encode('utf-8'))
+        self.do_send_message(json.dumps(brain_message))
         self.brains_data.clear()
 
-    # 启动服务端
     def start_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
             server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
@@ -125,12 +135,11 @@ class Revolve2Server:
         # main(self)
 
     def run_experiment_2(self):
-        # from revolve2.vr.examples.experiment_foundations.d_evaluate_multiple_interacting_robots.main import main
-        from revolve2.vr.main import main
+        from revolve2.vr.examples.experiment_foundations.d_evaluate_multiple_interacting_robots.main import main
         main(self)
 
-    def run_experiment_3(self):
-        from revolve2.vr.examples.robot_brain_cmaes.main import main
+    def run_experiment_3(self, main=None):
+        from revolve2.vr.examples.example_experiment_setups.e_robot_brain_cmaes.main import main
         main(self)
 
 server = Revolve2Server(HOST, PORT)
